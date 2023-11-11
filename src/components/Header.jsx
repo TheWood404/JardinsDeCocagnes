c2e44cb8638c84f7f9f12b1feb62b13c6b4b8e8b
Jules GOUEREC <jules.gouerec@gmail.com>
2023-11-11 16:11:41 +0100

    Header fini

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPagelines } from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
   return (
-    <header className="bg-white text-black p-4 flex justify-between items-center">
+    <header className="bg-white text-green-title p-4 flex justify-between items-center">
       <div>
         <a href="#"><img src='../images/CocagneLogo.png' width='150' height='100' alt='Logo des Jardins de Cocagnes'></img></a>
       </div>
       <nav className="flex">
-        <a href="#" className="mr-4">Point de dépôt</a>
-        <a href="#" className="mr-4">Produits</a>
-        <a href="#" className="mr-4">Calendrier</a>
-        <a href="#" className='mr-4'>Abonnement</a>
+        <a href="#" className="mr-36">Point de dépôt</a>
+        <a href="#" className="mr-36">Produits</a>
+        <a href="#" className="mr-36">Calendrier</a>
+        <a href="#" className='mr-36'>Abonnement</a>
       </nav>
-      <div className="text-2xl font-bold">
-        <a href="#">Nav Logo</a>
+      <div className="relative">
+        <div className="bg-green-title rounded-full p-4 w-14 h-14 absolute top-0 right-10 transform translate-x-1/2 -translate-y-1/2 flex items-center justify-center text-center">
+          <FontAwesomeIcon icon={faPagelines} size="2xl" style={{ color: "#ffffff" }} />
+        </div>
       </div>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-title': '#93C356',
+      },
+    },
   },
   plugins: [],
 }
