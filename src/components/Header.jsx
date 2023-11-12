b7e7d7d2da663da7a76babe43312f67e30ac1015
TheWood404 <vicgassperso@gmail.com>
2023-11-12 14:15:24 +0100

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPagelines } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Icônes pour le menu burger
 import { Link } from 'react-router-dom';
 
-
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className="bg-white text-green-title p-4 flex justify-between items-center">
       <div>
         <a><img src='../images/CocagneLogo.png' width='150' height='100' alt='Logo des Jardins de Cocagnes'></img></a>
       </div>
-      <nav className="flex">
-        <Link to="/point-de-depot" className="mr-36">Point de dépôt</Link>
-        <Link to="/produits" className="mr-36">Produits</Link>
-        <Link to="/calendrier" className="mr-36">Calendrier</Link>
-        <Link to="/abonnement" className="mr-36">Abonnement</Link>
-      </nav>
-      <div className="relative">
-        <div className="bg-green-title rounded-full p-4 w-14 h-14 absolute top-0 right-10 transform translate-x-1/2 -translate-y-1/2 flex items-center justify-center text-center">
+      <nav className="hidden md:flex items-center">
+        <div className="flex">
+          <Link to="/point-de-depot" className="mr-8">Point de dépôt</Link>
+          <Link to="/produits" className="mr-8">Produits</Link>
+          <Link to="/calendrier" className="mr-8">Calendrier</Link>
+          <Link to="/abonnement" className="mr-8">Abonnement</Link>
+        </div>
+        <div className="bg-green-title rounded-full p-4 w-14 h-14 transform flex items-center justify-center text-center">
           <Link to="/compte-strucure">
             <FontAwesomeIcon icon={faPagelines} size="2xl" style={{ color: "#ffffff" }} />
           </Link>
         </div>
+      </nav>
+      <div className="md:hidden">
+        <button onClick={toggleMenu}>
+          {menuOpen ? (
+            <FontAwesomeIcon icon={faTimes} className="text-green-title text-2xl" />
+          ) : (
+            <FontAwesomeIcon icon={faBars} className="text-green-title text-2xl" />
+          )}
+        </button>
       </div>
+      {menuOpen && (
+        <div className="md:hidden absolute top-16 right-4 bg-white p-4 border rounded-lg shadow-md">
+          <ul>
+            <li className="mb-2">
+              <Link to="/point-de-depot" onClick={toggleMenu}>Point de dépôt</Link>
+            </li>
+            <li className="mb-2">
+              <Link to="/produits" onClick={toggleMenu}>Produits</Link>
+            </li>
+            <li className="mb-2">
+              <Link to="/calendrier" onClick={toggleMenu}>Calendrier</Link>
+            </li>
+            <li className="mb-2">
+              <Link to="/abonnement" onClick={toggleMenu}>Abonnement</Link>
+            </li>
+            <li>
+              <Link to="/compte-structure" onClick={toggleMenu}>Compte Structure</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 };
 
-export default Header;  
+export default Header;
